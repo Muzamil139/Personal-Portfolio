f91d7aeba7bfe6d80545a0d81e3b01ddac392527
Muzamil Gustasib <96966338+Muzamil139@users.noreply.github.com>
2023-01-10 07:03:31 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+
+ let menuBox = document.getElementById("menuBox");
+ let menuIcon = document.getElementById("menuIcon");
+ 
+ menuIcon.onclick = function(){
+    menuBox.classList.toggle("open-menu");
+
+
+if(menuBox.classList.contains("open-menu")){
+    menuIcon.src = "images/close.png";
+}
+
+else{
+    menuIcon.src = "images/menu.png";
+}}
